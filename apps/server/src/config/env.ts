81602d9bf4f9e27dc8d3f0bccfe74b7d3f067b73
Abhishek <github.abhiarya@gmail.com>
2025-06-10 01:27:46 +0530

    added: t3-env

--- apps/server/src/config/env.ts
+++ apps/server/src/config/env.ts
@@ -3,9 +3,11 @@ import { z } from "zod";
 
 export const env = createEnv({
 	server: {
-		GOOGLE_CLIENT_ID: z.string().url(),
+		DATABASE_URL: z.string().url(),
+		GOOGLE_CLIENT_ID: z.string(),
 		GOOGLE_CLIENT_SECRET: z.string().min(1),
 		FRONTEND_URL: z.string().url(),
+		BACKEND_URL: z.string().url(),
 	},
 	runtimeEnv: process.env,
 	emptyStringAsUndefined: true,
