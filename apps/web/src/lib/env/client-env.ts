6b3f515178e68a05c8659837c045a1bb0879956a
Harsh <hp424805@gmail.com>
2025-06-25 22:46:42 +0530

    fix: build issues

--- apps/web/src/lib/env/client-env.ts
+++ apps/web/src/lib/env/client-env.ts
@@ -12,7 +12,8 @@ export const clientEnv = createEnv({
 			.url("NEXT_PUBLIC_CALLBACK_URL must be a valid URL (e.g., https://yourdomain.com)."),
 		NEXT_PUBLIC_FRONTEND_URL: z
 			.string({ message: "The NEXT_PUBLIC_FRONTEND_URL environment variable is required." })
-			.url("NEXT_PUBLIC_FRONTEND_URL must be a valid URL (e.g., https://yourdomain.com)."),
+			.url("NEXT_PUBLIC_FRONTEND_URL must be a valid URL (e.g., https://yourdomain.com)")
+			.optional(),
 	},
 	runtimeEnv: {
 		NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
